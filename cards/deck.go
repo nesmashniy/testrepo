e4b5547b5ea1cb26d1f5bbd0bff712db7931a0b7
Aleksandr Nesmashnyi <aleksandr@registry.godaddy>
2022-03-23 16:44:48 +1100

    Add reciever

---
+++ cards/deck.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Create a net type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
