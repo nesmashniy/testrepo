9f09ab3c23c5f1bcfe8e5c0f476393da8d9efb26
Aleksandr Nesmashnyi <aleksandr@registry.godaddy>
2022-04-08 14:48:32 +1000

    Write to file 1.0

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	// cards := newDeck()
+
+	// hand, remainingDeck := deal(cards, 5)
+
+	// hand.print()
+	//remainingDeck.print()
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	fmt.Println(cards.toString())
 }
